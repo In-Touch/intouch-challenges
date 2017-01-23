340b83c0af28b6295c29da6c15d47b4548a5763a
xP xP <xP xP>
2017-01-22 22:04:06 -0700

    Completed Challenges

--- Challenge 1 - Sorting/challenge1.php
+++ Challenge 1 - Sorting/challenge1.php
@@ -1,2 +1,45 @@
 <?php
 
+Class MakeSort{
+    private $numbers = array();
+    private $max;
+    private $min;
+    
+    function __construct(int $min,int $max){
+        $this->max = $max;
+        $this->min = $min;
+    }
+    
+    public function sortNumbers(){
+        //Even though it's set to 100 numbers, count in case it changes later.
+        $count = count($this->numbers);
+        
+        for($i = 0; $i < $count; $i++){
+            for($j = 0;$j < $count-1; $j++){
+                if($this->numbers[$j] > $this->numbers[$j+1]){
+                    $k = $this->numbers[$j+1];
+                    $this->numbers[$j+1] = $this->numbers[$j];
+                    $this->numbers[$j] = $k;
+                }
+            }
+        }
+        
+        return $this->numbers;
+    }
+    
+    public function genNum(){
+        for($i = 0; $i < 100; $i++){
+            //this highlights why we don't use rand or mt_rand for true randomness.
+            $this->numbers[] = rand($this->min,$this->max);
+        }
+        
+        return $this->numbers;
+    }
+}
+$class = new MakeSort(1,99);
+print_r('Unsorted List:');
+print_r($class->genNum());
+print_r('Sorted Numbers:');
+print_r($class->sortNumbers());
+
+?>
--- Challenge 3 - Cryptography/challenge3.php
+++ Challenge 3 - Cryptography/challenge3.php
@@ -1,2 +1,5 @@
 <?php
 
+//This one has got me stumped pretty good. 
+
+?>
